4bd3294c5d7b97b6fb61e0237350aaabf5958e6a
sidraya <sidhujayagond@gmail.com>
2023-11-09 09:41:42 +0530

    Add various architecture docs

--- code/conf.sh
+++
@@ -1,9 +0,0 @@
-bash configure \
-	--with-boot-jdk=~/jdk-20+36 \
-	--with-jtreg=../jtreg/build/images/jtreg/ \
-	--with-gtest=../googletest \
-	--with-hsdis=binutils \
-	--with-binutils-src=~/binutils-2.37/ \
-	--with-debug-level=slowdebug \
-	--with-native-debug-symbols=internal \
-	--with-num-cores=8
--- code/tt.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
- 	printf(":0x%lx\n", (unsigned long)(192L << 40 | 5L << 32));
-	return 0;
-}
